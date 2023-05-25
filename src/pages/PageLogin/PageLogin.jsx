f88b90b151163bf15a8be8d05e72cc8b96d5f734
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-05-25 18:27:26 -0300

    feat(Add): form with email and password inputs

--- src/pages/PageLogin/PageLogin.jsx
+++ src/pages/PageLogin/PageLogin.jsx
@@ -1,6 +1,33 @@
+import { Form, Login } from './Styles';
+
 const PageLogin = () => {
   return (
-    <div>PageLogin</div>
+    <Login>
+      <h1 className="title">Login System</h1>
+      <Form>
+        <div className="field">
+          <label htmlFor="email"></label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="Type your email" 
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="password"></label>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            placeholder="Type your password" 
+          />
+        </div>
+        <div className="actions">
+          <button type="submit">Sign In</button>
+        </div>
+      </Form>
+    </Login>
   )
 }
 
