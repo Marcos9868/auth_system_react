2f5f99ef54cd2e4070adac72292b646e50f2cd97
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-05-25 18:40:19 -0300

    feat(style): Form, Label, Input, Actions' components

--- src/pages/PageLogin/Styles.js
+++ src/pages/PageLogin/Styles.js
@@ -11,4 +11,24 @@ export const Login = styled.div`
 
 export const Form = styled.form`
   background-color: #f5f5f5;
+  width: 100%;
+  height: 480px;
+  padding: 0.5rem;
+`;
+
+export const Field = styled.div`
+  padding-bottom: 0.5rem;
+`;
+
+export const Label = styled.label`
+  display: block;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+`;
+
+export const Actions = styled.div`
+  text-align: center;
+  margin-top: 0.5rem;
 `;
