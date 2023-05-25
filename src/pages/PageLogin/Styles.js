8ad350a27fd1ad8d46891b28aa28086b553eba31
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-05-25 18:27:50 -0300

    feat(Add): basic form styles

---
+++ src/pages/PageLogin/Styles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 0 2rem;
+`;
+
+export const Form = styled.form`
+  background-color: #f5f5f5;
+`;
