a79596e64a9e421babbaf3b0ce945f5f5b3ec880
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-05-25 18:26:46 -0300

    fix: remove h1 and HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import HomePage from './pages/HomePage/HomePage'
+// import HomePage from './pages/HomePage/HomePage'
 import PageLogin from './pages/PageLogin/PageLogin'
 
 function App() {
   return (
     <>
-      <h1>Hello Dev</h1>
       <PageLogin />
-      <HomePage />
     </>
   )
 }
