d4312ccd88c3ea0d769e33edb8745a2a1f141e87
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-05-25 18:12:26 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HomePage from './pages/HomePage/HomePage'
+import PageLogin from './pages/PageLogin/PageLogin'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dev</h1>
+      <PageLogin />
+      <HomePage />
+    </>
+  )
+}
+
+export default App
