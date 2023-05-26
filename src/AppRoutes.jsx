65d3434c627bfa8f708cc9cdfda6f38fa79f8e13
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-05-26 17:58:08 -0300

    feat(Add): Login function and Authcontext Provider

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,14 +1,32 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageLogin from "./pages/PageLogin/PageLogin";
 import HomePage from "./pages/HomePage/HomePage";
+import { AuthContext } from "./context/Auth";
+import { useState } from "react";
+
+const user = [
+  { id: 123, email: 'teste@teste.com', password: '123456'}
+]
 
 const AppRoutes = () => {
+  const [user, setUser] = useState(null);
+
+  const login = (email, password) => {
+    console.log('login', { email, password });
+    setUser({ id: 123, email })
+  }
+
+  const logout = () => console.log('logout');
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/login" element={<PageLogin />} />
-        <Route path="/" element={<HomePage />}/>
-      </Routes>
+      <AuthContext.Provider value={{ authenticated: !!user, user, login, logout }}>
+        <Routes>
+          <Route exact path="/login" element={<PageLogin />} />
+          <Route path="/" element={<HomePage />}/>
+        </Routes>
+      </AuthContext.Provider>
     </BrowserRouter>
   )
 }
